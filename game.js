758b14e61c7e4afb73bae93ba2ffcbb47d7cb696
csodamate <118101016+csodamate@users.noreply.github.com>
2024-02-12 05:15:14 +0100

    Add files via upload

---
+++ game.js
@@ -0,0 +1,13 @@
+function cbence(){
+    document.getElementById('kep').src='bence.png';
+}
+function cmark(){
+    document.getElementById('kep').src='mark.png';
+}
+
+a = 0;
+function pontplus(){
+    a++;
+    document.getElementById('pont').innerHTML = a;
+}
+
